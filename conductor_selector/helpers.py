78fda8f62424d4eb3b3e93aa4f40196703e8d085
oscar monsalve <oscarmonsalvec5@gmail.com>
2025-03-16 15:18:28 -0500

    update

--- conductor_selector/helpers.py
+++ conductor_selector/helpers.py
@@ -1,33 +1,8 @@
-# Inputs:
-#   - power_system: single-phase or three-phase
-#   - voltage_from_trafo: 208 V, 214 V, or 220 V.
-#   - active_power P: in hp or watts
-#   - power_factor: 0.8 <= fp <= 1
-#   - DT: distance to the distribution board (DDB)
-#
-# Notes:
-#   - Maximum allowable current: <= 195 A. if higher, the program must throw an exception error.
-#   - Voltage drop: <= 3%. If higher, the program must throw an exception error.
-#   - DT distance: <= 100. If higher, the program must throw an exception error.
-#   - Calculations only for copper conductors at 60 °C.
-#   - Maximum caliber: 4/0 AWG.
-#
-# Outputs:
-#   - Number of the circtuit depending the type of power system: 1, 1-2, 1-2-3.
-#   - Voltage.
-#   - Current.
-#   - Thermomagnetic protection.
-#   - Number of poles.
-#   - DT distance.
-#   - Voltage drop.
-#   - Caliber of the phase conductor.
-#   - Caliber of the neutral conductor.
-#   - Caliber of the ground conductor.
-#   - Diameter of conduit in EMT and PCV type A.
-
 from math import sqrt
 
-# Conductor caliber for Copper at 60 °C, Table 310.15(B)(16), NTC 2050 segunda actualización, pag. 191.
+# TODO: add and program the selection of conduit EMT type.
+
+# Conductor caliber and capacity of current (ampacity) in amps for Copper at 60 °C, Table 310.15(B)(16), NTC 2050 segunda actualización, pag. 191.
 copper_caliber = {
     "14 AWG": 15,
     "12 AWG": 20,
@@ -44,7 +19,7 @@ copper_caliber = {
     "4/0 AWG": 195,
 }
 
-# Conductor resistance for Copper (coated, "recubierto") given in ohms/km, Table 8 NTC 2050, pag. 962.
+# Conductor resistance for Copper (coated, "recubierto") in ohms/km, Table 8 NTC 2050, pag. 962.
 conductor_resistance = {
     "14 AWG": 10.7,
     "12 AWG": 6.73,
@@ -60,6 +35,80 @@ conductor_resistance = {
     "3/0 AWG": 0.261,
     "4/0 AWG": 0.205,
 }
+
+# Nominal standard thermomagnetic protection values in amps, Table 240.6(A) NTC 2050, pag. 109.
+thermomagnetic_potection_values = [
+    15, 20, 25, 30, 35, 40, 45, 50, 60, 70, 80,
+    90, 100, 110, 125, 150, 175, 200, 225, 250,
+    300, 350, 400, 450, 500, 600, 700, 800, 1000,
+    1200, 1600, 2000, 2500, 3000, 4000, 5000, 6000
+]
+
+# Ground conductor minimum caliber for Copper, Table 250.122, NTC 2050 segunda actualización, pag. 154.
+copper_ground_caliber = {
+    "14 AWG": 15,
+    "12 AWG": 20,
+    "10 AWG": 60,
+    "8 AWG": 100,
+    "6 AWG": 200,
+    "4 AWG": 300,
+    "3 AWG": 400,
+    "2 AWG": 500,
+    "1 AWG": 600,
+    "1/0 AWG": 800,
+    "2/0 AWG": 1000,
+    "3/0 AWG": 1200,
+    "4/0 AWG": 1600,
+}
+
+# Single conductor's cross-sectional area in mm^2 (with isolation tpye THHN, THWN,f THWN-2), Table 5, NTC 2050 segunda actualización, pag. 958.
+single_conductor_area = {
+    "14 AWG": 6.258,
+    "12 AWG": 8.581,
+    "10 AWG": 13.61,
+    "8 AWG": 23.61,
+    "6 AWG": 32.71,
+    "4 AWG": 53.16,
+    "3 AWG": 62.77,
+    "2 AWG": 74.71,
+    "1 AWG": 100.8,
+    "1/0 AWG": 119.7,
+    "2/0 AWG": 143.4,
+    "3/0 AWG": 172.8,
+    "4/0 AWG": 208.8,
+}
+
+# PVC type A conduit's commercial diameter in inches for more than 2 threads at 40% depending on the total area of the conductors in mm^2,
+# Table 5, NTC 2050 segunda actualización, pag. 958.
+conduit_pvc_type_A_commercial_diameter = {
+    "1/2 inch": 100,
+    "3/4 inch": 168,
+    "1 inch": 279,
+    "1 1/4 inch": 456,
+    "1 1/2 inch": 600,
+    "2 inch": 940,
+    "2 1/2 inch": 1406,
+    "3 inch": 2112,
+    "3 1/2 inch": 2758,
+    "4 inch": 3543,
+}
+
+
+def power_system_information(power_system_type: int) -> [str, str, int, int, int]:
+    # TODO: add the variable "conduit type: str" for PVC type A or EMT
+    if power_system_type == 1:
+        circuit = "1"
+        system_type = "single-phase"
+        number_of_phases = 1
+        total_number_of_conductors = 3
+        number_of_poles = 1
+    if power_system_type == 3:
+        circuit = "1-2-3"
+        system_type = "three-phase"
+        number_of_phases = 3
+        total_number_of_conductors = 5
+        number_of_poles = 3
+    return circuit, system_type, number_of_phases, total_number_of_conductors, number_of_poles
 
 
 def check_power_system_type(power_system_type: float) -> None:
@@ -136,29 +185,80 @@ def load_current(power_system_type: int, apparent_power: float, voltage_level: f
         return apparent_power / (sqrt(3) * voltage_level)
 
 
-def find_caliber(load_current: float) -> dict[str, int]:
+def find_caliber(load_current: float) -> list[tuple[str, int]]:
+    caliber_list = []
     for caliber, nominal_caliber_current in copper_caliber.items():
         if load_current <= nominal_caliber_current:
-            return caliber, nominal_caliber_current
+            caliber_list.append((caliber, nominal_caliber_current))
+    return caliber_list
 
 
 def find_conductor_resistance(phase_caliber: str) -> float:
-    for caliber, resistance in conductor_resistance.items():
-        if phase_caliber == caliber:
-            return resistance
+    return conductor_resistance.get(phase_caliber, None)
 
 
 def voltage_drop(power_system_type: str, voltage_level: float, load_current: float, distance_to_board: float,
                  conductor_resistance: float, nc=1) -> float:
+    if voltage_level == 0:
+        raise ValueError("Voltage level cannot be zero. Othewise there will be division by zero")
     if power_system_type == 1:
         return (2 * distance_to_board * conductor_resistance * load_current) / (1000 * voltage_level * nc) * 100
     if power_system_type == 3:
         return (sqrt(3) * distance_to_board * conductor_resistance * load_current) / (1000 * voltage_level * nc) * 100
+    else:
+        raise ValueError("Invalid power system type. Use 1 (single-phase) or 3 (three-phase)")
 
 
-def find_thermoelectric_protection():
-    pass
+def select_proper_caliber(load_current: float, power_system_type: int, voltage_level: float,
+                          distance_to_board: float, nc=1) -> tuple[str, float]:
+    # List of calibers that can handle the load current
+    caliber_options = find_caliber(load_current)
+
+    for caliber, nominal_current in caliber_options:
+        conductor_resistance = find_conductor_resistance(caliber)
+        if conductor_resistance is None:
+            raise ValueError(f"No resistance data available for {caliber}.")
+
+        drop = voltage_drop(power_system_type, voltage_level, load_current, distance_to_board, conductor_resistance, nc)
+        if drop <= 3:
+            return caliber, nominal_current, drop
+
+    raise ValueError(f"No available conductor meets the voltage drop requierement. With a caliber of {caliber}, the voltage drop is {drop:.2f}%.")
 
 
-def find_ground_caliber():
+def find_thermoelectric_protection(load_current: float, caliber: str) -> int:
+    if load_current > 195:
+        raise ValueError(f"The load current ({load_current} A) surpasses the limit current of 195 A.")
+    elif caliber == "14 AWG":
+        return 15
+    elif caliber == "12 AWG":
+        return 20
+    elif caliber == "10 AWG":
+        return 30
+    else:
+        for i in reversed(thermomagnetic_potection_values):
+            if i <= load_current:
+                return i
+
+
+def find_ground_caliber(thermomagnetic_protection_current: int) -> str:
+    for ground_caliber, nominal_current in list(copper_ground_caliber.items()):
+        if nominal_current > thermomagnetic_protection_current:
+            return ground_caliber
+
+
+def find_phase_caliber_cross_sectional_area(phase_caliber: str) -> float:
+    return single_conductor_area.get(phase_caliber, None)
+
+
+def find_conduit_diameter_pvc_type_A(total_conductor_area: float) -> str:
+    if total_conductor_area > 3543:
+        raise ValueError(f"No commercial conduit available in PVC type A for {total_conductor_area} mm^2 because is greater than 3543 mm^2.")
+    else:
+        for commercial_diameter, conduit_standard_area in list(conduit_pvc_type_A_commercial_diameter.items()):
+            if conduit_standard_area >= total_conductor_area:
+                return commercial_diameter
+
+
+def find_conduit_diameter_emt():
     pass
--- conductor_selector/main.py
+++ conductor_selector/main.py
@@ -1,12 +1,40 @@
+# Inputs:
+#   - power_system: single-phase or three-phase
+#   - voltage_from_trafo: 208 V, 214 V, or 220 V.
+#   - active_power P: in hp or watts
+#   - power_factor: 0.8 <= fp <= 1
+#   - DT: distance to the distribution board (DDB)
+#
+# Notes:
+#   - Maximum allowable current: <= 195 A. if higher, the program must throw an exception error.
+#   - Voltage drop: <= 3%. If higher, the program must throw an exception error.
+#   - DT distance: <= 100. If higher, the program must throw an exception error.
+#   - Calculations only for copper conductors at 60 °C.
+#   - Maximum caliber: 4/0 AWG.
+#
+# Outputs:
+#   - Number of the circtuit depending the type of power system: 1, 1-2, 1-2-3.
+#   - Voltage.
+#   - Current.
+#   - Thermomagnetic protection.
+#   - Number of poles.
+#   - DT distance.
+#   - Voltage drop.
+#   - Caliber of the phase conductor.
+#   - Caliber of the neutral conductor.
+#   - Caliber of the ground conductor.
+#   - Diameter of conduit in EMT and PCV type A.
+
 import helpers as h
 
-# Inputs
-POWER_SYSTEM_TYPE:   int = 3  # 1 if single-phase. 3 if three-phase
-TRAFO_VOLTAGE:       int = 220  # 208 V, 214 V, or 220 V.
-ACTIVE_POWER:      float = 20
-ACTIVE_POWER_UNIT:   str = "hp"  # "watts", "hp", or "cv".
-POWER_FACTOR:      float = 0.85  # 0.8 <= fp <= 1
-DT:                float = 70  # distance to the distribution board in meters.
+# ---- Inputs ----
+POWER_SYSTEM_TYPE:   int = 3     # 1 (single-phase) or 3 (three-phase).
+TRAFO_VOLTAGE:       int = 220   # Transformer voltage: 208 V, 214 V, or 220 V.
+ACTIVE_POWER:      float = 20    # Active power in watts, hp or cv.
+ACTIVE_POWER_UNIT:   str = "hp"  # Active power unit as "watts", "hp", or "cv".
+POWER_FACTOR:      float = 0.85  # Range (0.8 <= power factor <= 1)
+DT:                float = 70    # distance to the distribution board in meters.
+# ---- Inputs ----
 
 
 def main() -> None:
@@ -16,37 +44,49 @@ def main() -> None:
     h.check_power_factor(POWER_FACTOR)
     h.check_dt_distance(DT)
 
+    # Power system information
+    (
+        circuit,
+        system_type,
+        number_of_phases,
+        total_number_of_conductors,
+        number_of_poles,
+    ) = h.power_system_information(POWER_SYSTEM_TYPE)
+
     # Initiate calculations
     voltage_level = h.voltage_level(POWER_SYSTEM_TYPE, TRAFO_VOLTAGE)
     active_power_watts = h.active_power_conversion(ACTIVE_POWER, ACTIVE_POWER_UNIT)
     apparent_power = h.apparent_power(active_power_watts, POWER_FACTOR)
     load_current = h.load_current(POWER_SYSTEM_TYPE, apparent_power, voltage_level)
     h.check_load_current(load_current)
-    phase_caliber, nominal_caliber_current = h.find_caliber(load_current)
-    conductor_resistance = h.find_conductor_resistance(phase_caliber)
-    voltage_drop = h.voltage_drop(POWER_SYSTEM_TYPE, voltage_level, load_current, DT, conductor_resistance)
 
-    # if voltage_drop > 2:
-    #     current_index = list(h.copper_caliber).index(phase_caliber)
-    #     for i,
+    (
+        # TODO: add the variable "conduit type: str" for PVC type A or EMT and replace it in the printing of "Conduit commercial diameter"
+        selected_phase_caliber,
+        nominal_current_selected_caliber,
+        final_voltage_drop
+    ) = h.select_proper_caliber(load_current, POWER_SYSTEM_TYPE, voltage_level, DT)
+
+    thermoelectric_protection = h.find_thermoelectric_protection(load_current, selected_phase_caliber)
+    selected_ground_caliber = h.find_ground_caliber(thermoelectric_protection)
+    area_of_highest_caliber = h.find_phase_caliber_cross_sectional_area(selected_phase_caliber)
+    total_conductor_area = total_number_of_conductors * area_of_highest_caliber
+    conduit_diameter_pvc_type_A = h.find_conduit_diameter_pvc_type_A(total_conductor_area)
 
     # Print results
-    if POWER_SYSTEM_TYPE == 1:
-        system_type = "single-phase"
-        number_of_phases = 1
-    if POWER_SYSTEM_TYPE == 3:
-        system_type = "three-phase"
-        number_of_phases = 3
-
-    print(f"Sytem type          : {system_type}")
-    print(f"Sytem voltage       : {voltage_level} V.")
-    print(f"Active power        : {active_power_watts:.2f} W.")
-    print(f"Apparent power      : {apparent_power:.2f} VA.")
-    print(f"Load current        : {load_current:.2f} A.")
-    print(f"Phase caliber       : {number_of_phases} x {phase_caliber} ({nominal_caliber_current} A).")
-    print(f"Neutral caliber     : 1 x {phase_caliber} ({nominal_caliber_current} A).")
-    print(f"Conductor resistance: {conductor_resistance} ohm/km.")
-    print(f"Voltage drop: {voltage_drop:.2f} %.")
+    print(f"System type                 : {system_type}")
+    print(f"System voltage              : {voltage_level:.2f} V.")
+    print(f"Active power                : {active_power_watts:.2f} W.")
+    print(f"Apparent power              : {apparent_power:.2f} VA.")
+    print(f"Load current                : {load_current:.2f} A.")
+    print(f"Thermoelectric protection   : {thermoelectric_protection} A.")
+    print(f"Voltage drop                : {final_voltage_drop:.2f} % (for {selected_phase_caliber}).")
+    print(f"Phase caliber               : {number_of_phases} x {selected_phase_caliber} ({nominal_current_selected_caliber} A).")
+    print(f"Neutral caliber             : 1 x {selected_phase_caliber}.")
+    print(f"Ground caliber              : 1 x {selected_ground_caliber}.")
+    print(f"Conductor individual area   : {area_of_highest_caliber} mm^2 ({selected_phase_caliber}).")
+    # print(f"Total conductor area        : {total_conductor_area:.2f} mm^2.")
+    print(f"Conduit commercial diameter : {conduit_diameter_pvc_type_A} (PVC type A).")
 
 
 if __name__ == "__main__":
