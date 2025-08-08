63e43207b9b9064b02c5ca2e862f2808f81873c4
oscar monsalve <oscarmonsalvec5@gmail.com>
2025-08-08 12:18:53 -0500

    update

--- tests/test_06_load_current.py
+++ tests/test_06_load_current.py
@@ -1,18 +1,13 @@
 from conductor_selector.helpers import load_current
 
 
-def test_load_current_single_phase_result():
-    POWER_SYSTEM_TYPE_SINGLE_PHASE = 1
+def test_load_current_single_two_phase_result():
     APPARENT_POWER = 2000
     VOLTAGE_LEVEL = 220
-    assert load_current(POWER_SYSTEM_TYPE_SINGLE_PHASE, APPARENT_POWER, VOLTAGE_LEVEL) == 9.090909090909092
+    power_system_type_single_two_phase = [1, 2]
 
-
-def test_load_current_two_phase_result():
-    POWER_SYSTEM_TYPE_TWO_PHASE = 2
-    APPARENT_POWER = 2000
-    VOLTAGE_LEVEL = 220
-    assert load_current(POWER_SYSTEM_TYPE_TWO_PHASE, APPARENT_POWER, VOLTAGE_LEVEL) == 9.090909090909092
+    for i in power_system_type_single_two_phase:
+        assert load_current(i, APPARENT_POWER, VOLTAGE_LEVEL) == 9.090909090909092
 
 
 def test_load_current_three_phase_result():
---
+++ tests/test_07_find_caliber.py
@@ -0,0 +1,7 @@
+from conductor_selector.helpers import find_caliber
+
+LOAD_CURRENT = 20
+
+
+def test_find_caliber():
+    pass
